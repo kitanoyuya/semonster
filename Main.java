e675930195ddd4814a43f1b6ba960ed73f93ca50
kitanoyuya <yu_1999_ya@ezweb.ne.jp>
2022-06-19 00:13:45 +0900

    main競合修正

--- Main.java
+++ Main.java
@@ -1,18 +1,17 @@
 
 public class Main {
   public static void main(String[] args) {
-<<<<<<< HEAD
-    System.out.println("SEMonster!");
-    Monster monster = new Monster();
-	Player player = new Player();
-	player.drawMonsters();
-	player.showDeck();
-	System.out.println(monster.toString());
 
-=======
-    System.out.println("SEMONSTER!!");
+    System.out.println("SEMonster!!");
     Monster monster = new Monster();
+    Player player = new Player();
+    player.drawMonsters();
+    player.showDeck();
     System.out.println(monster.toString());
->>>>>>> main
+
+    // System.out.println("SEMONSTER!!");
+    // Monster monster = new Monster();
+    // System.out.println(monster.toString());
+
   }
 }
